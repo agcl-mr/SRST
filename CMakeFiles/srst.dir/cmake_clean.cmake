67372c57af469db523a165f0189ddc6417e06521
Advanced Geometric Computing Lab <agcl.mr@gmail.com>
2022-06-14 14:13:01 +0530

    Add files via upload

---
+++ CMakeFiles/srst.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/srst.dir/srst.cpp.o"
+  "srst.pdb"
+  "srst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/srst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
